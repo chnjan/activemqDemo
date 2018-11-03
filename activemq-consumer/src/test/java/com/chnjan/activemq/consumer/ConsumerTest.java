a3cd1885c7292d32fa28f9ffe251d346f6723675
chnjan <30980930+chnjan@users.noreply.github.com>
2018-11-03 17:52:42 +0800

    topic消息，及持久订阅模式

--- activemq-consumer/src/test/java/com/chnjan/activemq/consumer/ConsumerTest.java
+++ activemq-consumer/src/test/java/com/chnjan/activemq/consumer/ConsumerTest.java
@@ -14,6 +14,7 @@ import javax.jms.MessageListener;
 import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.TextMessage;
+import javax.jms.Topic;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.junit.Test;
@@ -25,6 +26,7 @@ import org.junit.Test;
  */
 public class ConsumerTest {
 
+	//queue消息消费者测试
 	@Test
 	public void queueConsumerTest() throws JMSException {
 		//消息服务器地址
@@ -60,7 +62,154 @@ public class ConsumerTest {
 		});
 		
 		try {
-			//阻塞主线程，保证测试一直处于消息监听状态，输入任意键后关闭资源
+			//阻塞主线程，保证测试一直处于消息监听状态，输入任意键后执行下面的关闭资源代码
+			System.in.read();
+			//关闭资源
+			consumer.close();
+			session.close();
+			connection.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//topic消息消费者1测试
+	@Test
+	public void topicConsumerTest() throws JMSException {
+		//消息服务器地址
+		String brokerUrl = "tcp://127.0.0.1:61616";
+		//创建一个连接工厂对象
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+		//创建一个connection连接对象
+		Connection connection = connectionFactory.createConnection();
+		//开启连接
+		connection.start();
+		//创建会话,参数：true开启事务，第二个参数会忽略；false不使用事务，第二个参数为消息的应答模式,自动应答
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		//创建一个消息主题,参数为消息主题的名字,名字要和发送时的消息队列名相同
+		Topic topic = session.createTopic("test mq topic");
+		//创建一个消息消费者,参数为指定的topic
+		MessageConsumer consumer = session.createConsumer(topic);
+		//设置消息监听,参数为匿名内部类
+		consumer.setMessageListener(new MessageListener() {
+			
+			@Override
+			public void onMessage(Message message) {
+				if (message instanceof TextMessage) {
+					TextMessage msg = (TextMessage) message;
+					try {
+						String textMsg = msg.getText();
+						System.out.println("消息topic1内容：");
+						System.out.println(textMsg);
+					} catch (JMSException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		
+		try {
+			//阻塞主线程，保证测试一直处于消息监听状态，输入任意键后执行下面的关闭资源代码
+			System.in.read();
+			//关闭资源
+			consumer.close();
+			session.close();
+			connection.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//topic消息消费者2测试
+	@Test
+	public void topicConsumerTest2() throws JMSException {
+		//消息服务器地址
+		String brokerUrl = "tcp://127.0.0.1:61616";
+		//创建一个连接工厂对象
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+		//创建一个connection连接对象
+		Connection connection = connectionFactory.createConnection();
+		//开启连接
+		connection.start();
+		//创建会话,参数：true开启事务，第二个参数会忽略；false不使用事务，第二个参数为消息的应答模式,自动应答
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		//创建一个消息主题,参数为消息主题的名字,名字要和发送时的消息队列名相同
+		Topic topic = session.createTopic("test mq topic");
+		//创建一个消息消费者,参数为指定的topic
+		MessageConsumer consumer = session.createConsumer(topic);
+		//设置消息监听,参数为匿名内部类
+		consumer.setMessageListener(new MessageListener() {
+			
+			@Override
+			public void onMessage(Message message) {
+				if (message instanceof TextMessage) {
+					TextMessage msg = (TextMessage) message;
+					try {
+						String textMsg = msg.getText();
+						System.out.println("消息topic2内容：");
+						System.out.println(textMsg);
+					} catch (JMSException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		
+		try {
+			//阻塞主线程，保证测试一直处于消息监听状态，输入任意键后执行下面的关闭资源代码
+			System.in.read();
+			//关闭资源
+			consumer.close();
+			session.close();
+			connection.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//topic消息持久订阅消费者测试
+	@Test
+	public void topicDurableConsumerTest() throws JMSException {
+		//消息服务器地址
+		String brokerUrl = "tcp://127.0.0.1:61616";
+		//创建一个连接工厂对象
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+		//创建一个connection连接对象
+		Connection connection = connectionFactory.createConnection();
+		//持久消费者需要设置订阅者id
+		connection.setClientID("001subscriber");
+		//开启连接
+		connection.start();
+		//创建会话,参数：true开启事务，第二个参数会忽略；false不使用事务，第二个参数为消息的应答模式,自动应答
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		//创建一个消息主题,参数为消息主题的名字,名字要和发送时的消息队列名相同
+		Topic topic = session.createTopic("test mq topic");
+		//创建一个消息消费者,参数为指定的topic
+		MessageConsumer consumer = //session.createConsumer(topic);
+		session.createDurableSubscriber(topic, "001subscriber");
+		//设置消息监听,参数为匿名内部类
+		consumer.setMessageListener(new MessageListener() {
+			
+			@Override
+			public void onMessage(Message message) {
+				if (message instanceof TextMessage) {
+					TextMessage msg = (TextMessage) message;
+					try {
+						String textMsg = msg.getText();
+						System.out.println("消息topic 持久订阅内容：");
+						System.out.println(textMsg);
+					} catch (JMSException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		
+		try {
+			//阻塞主线程，保证测试一直处于消息监听状态，输入任意键后执行下面的关闭资源代码
 			System.in.read();
 			//关闭资源
 			consumer.close();
--- activemq-producer/src/test/java/com/chnjan/activemq/producer/ProducerTest.java
+++ activemq-producer/src/test/java/com/chnjan/activemq/producer/ProducerTest.java
@@ -5,11 +5,13 @@ package com.chnjan.activemq.producer;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+//import javax.jms.DeliveryMode;
 import javax.jms.JMSException;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.TextMessage;
+import javax.jms.Topic;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.junit.Test;
@@ -21,7 +23,7 @@ import org.junit.Test;
  */
 public class ProducerTest {
 
-	
+	//queue消息发送测试
 	@Test
 	public void queueProducerTest() throws JMSException {
 		//消息服务器地址
@@ -40,6 +42,36 @@ public class ProducerTest {
 		TextMessage textMessage = session.createTextMessage("a hello message 3");
 		//创建一个消息发送对象
 		MessageProducer producer = session.createProducer(queue);
+		//发送
+		producer.send(textMessage);
+		//关闭资源
+		producer.close();
+		session.close();
+		connection.close();
+	}
+	
+	
+	//topic消息发送测试
+	@Test
+	public void topicProducerTest() throws JMSException {
+		//消息服务器地址
+		String brokerUrl = "tcp://127.0.0.1:61616";
+		//创建一个连接工厂对象
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+		//创建一个connection连接对象
+		Connection connection = connectionFactory.createConnection();
+		//开启连接
+		connection.start();
+		//创建会话,参数：true开启事务，第二个参数会忽略；false不使用事务，第二个参数为消息的应答模式,自动应答
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		//创建一个消息topic,参数为消息主题的名字
+		Topic topic = session.createTopic("test mq topic");
+		//创建一条文本消息
+		TextMessage textMessage = session.createTextMessage("a hello topic message1");
+		//创建一个消息发送对象
+		MessageProducer producer = session.createProducer(topic);
+		//设置消息的发送模式，不设置默认就是持久模式
+		//producer.setDeliveryMode(DeliveryMode.PERSISTENT);
 		//发送
 		producer.send(textMessage);
 		//关闭资源
